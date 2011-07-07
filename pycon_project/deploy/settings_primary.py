ee7e0196624692cfabc3a9bfa99671274e04238c
Luke Hatcher <lukeman@gmail.com>
2011-07-07 16:05:24 -0400

    add email settings

--- pycon_project/deploy/settings_primary.py
+++ pycon_project/deploy/settings_primary.py
@@ -1,3 +1,10 @@
 MEDIA_URL = "/2012/site_media/media/"
 STATIC_URL = "/2012/site_media/static/"
 ADMIN_MEDIA_PREFIX = "/2012/site_media/static/admin/"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "sendgrid@eldarion.com"
+EMAIL_HOST_PASSWORD = "at8di4op5va"
+EMAIL_USE_TLS = True
